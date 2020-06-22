74aaf1fc6835208d97134b8578fb66b8a6f55147
Joy <joygupta501@gmail.com>
2020-06-22 15:56:24 +0530

    Final Call (Removed Errors) 🧐

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -7,44 +7,29 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
     start_y *= 0.01;
     end_x *= 0.01;
     end_y *= 0.01;
-
-    // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
-    // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
-    //start_node & end_node are the objects.
-    // '&' returns the address
-    start_node = &m_Model.FindClosestNode(start_x, start_y);
-    end_node = &m_Model.FindClosestNode(end_x, end_y);
-
+    
+    start_node = &m_Model.FindClosestNode(start_x, start_y);  //Closest Node to Start Node
+    end_node = &m_Model.FindClosestNode(end_x, end_y);     //Closest Node to End Node
 
 }
 
 
-// TODO 3: Implement the CalculateHValue method.
-// Tips:
-// - You can use the distance to the end_node for the h value.
-// - Node objects have a distance method to determine the distance to another node.
-//As mentioned to use end_node for distance calculation
+
 float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
-
-	return(std::distance(*end_node)); //Dereferencing end_node
+     //Dereferencing end_node
+	return(end_node -> distance(*node)); //Returning distance of node from end_node
 
 }
 
 
-// TODO 4: Complete the AddNeighbors method to expand the current node by adding all unvisited neighbors to the open list.
-// Tips:
-// - Use the FindNeighbors() method of the current_node to populate current_node.neighbors vector with all the neighbors.
-// - For each node in current_node.neighbors, set the parent, the h_value, the g_value. 
-// - Use CalculateHValue below to implement the h-Value calculation.
-// - For each node in current_node.neighbors, add the neighbor to open_list and set the node's visited attribute to true.
 
 void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
 	current_node -> FindNeighbors(); //Calling FindNeighbors() using object current_node
 	for(auto single_neighbor : current_node -> neighbors) //iterating over each node 
 	{
 		single_neighbor -> parent = current_node; //Setting up parent
-		single_neighbor -> g_value = current_node -> g_value + current_node ->distance(single_neighbor); //Setinng up gvalue
-		single_neighbor -> h_value = CalculateHValue(single_neighbor); //Setting up fvalue
+        single_neighbor -> h_value = CalculateHValue(single_neighbor); //Setting up fvalue
+		single_neighbor -> g_value = current_node -> g_value + current_node ->distance(*single_neighbor); //Setinng up gvalue
 		open_list.push_back(single_neighbor); //Pushing single neighbor back to open_list
 		single_neighbor -> visited = true; //Setting up visited attribute to ture.	
 
@@ -60,30 +45,18 @@ bool Compare2Nodes(const RouteModel::Node *Node1, const RouteModel::Node *Node2)
 	return (((Node1 -> g_value + Node1 -> h_value) > (Node2 -> g_value + Node2 -> h_value)) ? true : false);
 }
 
-// TODO 5: Complete the NextNode method to sort the open list and return the next node.
-// Tips:
-// - Sort the open_list according to the sum of the h value and g value.
-// - Create a pointer to the node in the list with the lowest sum.
-// - Remove that node from the open_list.
-// - Return the pointer.
+
 
 RouteModel::Node *RoutePlanner::NextNode() {
-	sort(open_list.begin(), open_list.back(), Compare2Nodes); //Sorting open_list using Compare2Nodes Function
+	sort(open_list.begin(), open_list.end(), Compare2Nodes); //Sorting open_list using Compare2Nodes Function
 	RouteModel::Node *Least = open_list.back(); //Just like the current node from the pervious exercise ie the one having least F_value
 	open_list.pop_back();// Remove it from open_list as it is no longer for available for visit
-	return least;
+	return Least;
 
 
 }
 
 
-// TODO 6: Complete the ConstructFinalPath method to return the final path found from your A* search.
-// Tips:
-// - This method should take the current (final) node as an argument and iteratively follow the 
-//   chain of parents of nodes until the starting node is found.
-// - For each node in the chain, add the distance from the node to its parent to the distance variable.
-// - The returned vector should be in the correct order: the start node should be the first element
-//   of the vector, the end node should be the last element.
 
 std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node *current_node) {
     // Create path_found vector
@@ -92,11 +65,11 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
 
 
     // TODO: Implement your solution here.
-    while (current_node -> parent = nullptr)
+    while (current_node -> parent != nullptr)
     {
     	path_found.push_back(*current_node); // Adding current_node to path_found vector
     	distance += current_node -> distance(*(current_node -> parent)); // Adding up the distance btw current node and current node's parent
-    	current_node = current_node -> parent; // Moving on to the parent node
+    	current_node = current_node -> parent;
     }
 
     path_found.push_back(*current_node); // Adding up last node
@@ -108,12 +81,6 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
 }
 
 
-// TODO 7: Write the A* Search algorithm here.
-// Tips:
-// - Use the AddNeighbors method to add all of the neighbors of the current node to the open_list.
-// - Use the NextNode() method to sort the open_list and return the next node.
-// - When the search has reached the end_node, use the ConstructFinalPath method to return the final path that was found.
-// - Store the final path in the m_Model.path attribute before the method exits. This path will then be displayed on the map tile.
 
 void RoutePlanner::AStarSearch() {
 
@@ -128,10 +95,8 @@ void RoutePlanner::AStarSearch() {
     	if(current_node == end_node) 
     	{
     		m_Model.path = ConstructFinalPath(current_node);
+            return;
     	}
 
     }
-
-    // TODO: Implement your solution here.
-
 }
